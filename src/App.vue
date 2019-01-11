034e15a8f5f94d3a4c8dba81cf66c469161c8c4e
Mike Copanos <mcopanos@gmail.com>
2019-01-10 23:04:58 -0600

    calculator app using vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Welcome/>
+    <Calculator/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
+import Welcome from './components/Welcome.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculator,
+    Welcome
   }
 }
 </script>
@@ -23,6 +25,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: aqua;
+  height: 100vh
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <h1>{{ msgOne }}</h1>
+        <h2>{{ msgTwo }}</h2>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msgOne: 'Welcome to my calculator app!!',
+            msgTwo: 'This app was created using Vue.js'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .header {
+        background-color: #111ccc;
+        color: aqua;
+    }
+</style>
